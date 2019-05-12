330354b5b0a7a34d193567c4f7d0f42d87b42e2f
selmirafernandes <selmira995@gmail.com>
2019-05-12 23:57:10 +0100

    Get e set da tabela inicio

--- app/src/main/java/com/example/cicles/dataInicio.java
+++ app/src/main/java/com/example/cicles/dataInicio.java
@@ -6,6 +6,19 @@ public class dataInicio {
     private String data;
 
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
