b9b2d220f6ad511a13b8af8864dd508c35027930
selmirafernandes <selmira995@gmail.com>
2019-05-12 23:59:17 +0100

    Get e set da tabela Fim

---
+++ app/src/main/java/com/example/cicles/dataFim.java
@@ -0,0 +1,23 @@
+package com.example.cicles;
+
+public class dataFim {
+
+    private int id;
+    private String DataF;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDataF() {
+        return DataF;
+    }
+
+    public void setDataF(String dataF) {
+        DataF = dataF;
+    }
+}
